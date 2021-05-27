f0ff0f712fd14489aff6498a0618bc96122eb07c
yeongbin-gong <youngbin816@gmail.com>
2021-05-27 11:11:47 +0900

    redis lock 수정

--- src/tracking/tracking.module.ts
+++ src/tracking/tracking.module.ts
@@ -4,12 +4,12 @@ import { Campaign } from 'src/entities/Campaign';
 import { SubMedia } from 'src/entities/SubMedia';
 import { TrackingController } from './tracking.controller';
 import { TrackingService } from './tracking.service';
-import { RedisLockModule } from 'nestjs-simple-redis-lock';
+// import { RedisLockModule } from 'nestjs-simple-redis-lock';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Campaign, SubMedia]),
-    RedisLockModule.register({}),
+    // RedisLockModule.register({}),
   ],
   controllers: [TrackingController],
   providers: [TrackingService],
