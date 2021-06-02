17b39f6f632184171cb2cc17a0dfe7426260dc25
yeongbin-gong <youngbin816@gmail.com>
2021-06-02 09:48:15 +0900

    eb deploy test

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,11 @@ import { PostbackModule } from './postback/postback.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { RedisModule } from 'nestjs-redis';
-const ENV = process.env.NODE_ENV;
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: !ENV ? '.env.dev' : `.env.dev`,
+      envFilePath: ['.env.dev', '.env.prod'],
       isGlobal: true,
     }),
     RedisModule.register({
