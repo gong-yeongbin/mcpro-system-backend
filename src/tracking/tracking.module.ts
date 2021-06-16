838ec5ab1654a4514b5608bf37bbbc5e3ad72742
yeongbin-gong <youngbin816@gmail.com>
2021-06-16 18:34:46 +0900

    ?

--- src/tracking/tracking.module.ts
+++ src/tracking/tracking.module.ts
@@ -4,13 +4,9 @@ import { Campaign } from 'src/entities/Campaign';
 import { TrackingController } from './tracking.controller';
 import { TrackingService } from './tracking.service';
 import { RedisLockModule } from 'nestjs-simple-redis-lock';
-import { PostBackDaily } from 'src/entities/PostBackDaily';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([Campaign, PostBackDaily]),
-    RedisLockModule.register({}),
-  ],
+  imports: [TypeOrmModule.forFeature([Campaign]), RedisLockModule.register({})],
   controllers: [TrackingController],
   providers: [TrackingService],
 })
