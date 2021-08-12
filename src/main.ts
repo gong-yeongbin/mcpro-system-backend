d4cf3df3fb27d9df2987dfe27ce4e9c700e6f259
yeongbin-gong <youngbin816@gmail.com>
2021-08-12 15:01:49 +0900

    eb pm2 add

--- src/main.ts
+++ src/main.ts
@@ -19,5 +19,5 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT || 3000);
 }
-bootstrap();
-// AppClusterService.clusterize(bootstrap);
+// bootstrap();
+AppClusterService.clusterize(bootstrap);
