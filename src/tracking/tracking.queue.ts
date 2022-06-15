23611acfda814fb7395e63af6d0922b480d962de
yeongbin-gong <youngbin816@gmail.com>
2022-06-15 11:50:19 +0900

    mongodb upgrade

--- src/tracking/tracking.queue.ts
+++ src/tracking/tracking.queue.ts
@@ -1,18 +1,13 @@
-import { InjectQueue, OnQueueActive, Process, Processor } from '@nestjs/bull';
+import { Process, Processor } from '@nestjs/bull';
 import { InjectModel } from '@nestjs/mongoose';
-import { Job, Queue } from 'bull';
+import { Job } from 'bull';
+import * as moment from 'moment-timezone';
 import { Model } from 'mongoose';
-import { Redis } from 'ioredis';
-import { RedisService } from 'nestjs-redis';
 import { Daily, DailyDocument } from 'src/schema/daily';
 
 @Processor('tracking')
 export class TrackingQueue {
-  constructor(
-    private readonly redisService: RedisService,
-    @InjectModel(Daily.name) private readonly dailyModel: Model<DailyDocument>,
-    @InjectQueue('tracking') private readonly trackingQueue: Queue,
-  ) {}
+  constructor(@InjectModel(Daily.name) private readonly dailyModel: Model<DailyDocument>) {}
 
   @Process('click')
   async handleClick(job: Job) {
@@ -21,25 +16,19 @@ export class TrackingQueue {
     const sub_id: string = job.data.sub_id;
     const impressionCode: string = job.data.impressionCode;
 
-    const redis: Redis = this.redisService.getClient();
-
-    const getClickCount: number = +(await redis.get(`${impressionCode}`));
-
-    if (!getClickCount) {
-      await redis.set(`${impressionCode}`, 1);
-    } else {
-      await redis.incr(`${impressionCode}`);
-    }
-
-    // await this.dailyModel.findOneAndUpdate(
-    //   {
-    //     token: token,
-    //     pub_id: pub_id,
-    //     sub_id: sub_id,
-    //     impressionCode: impressionCode,
-    //   },
-    //   { $inc: { click: 1 } },
-    //   { upsert: true },
-    // );
+    await this.dailyModel.findOneAndUpdate(
+      {
+        token: token,
+        pub_id: pub_id,
+        sub_id: sub_id,
+        impressionCode: impressionCode,
+        createdAt: {
+          $gte: `${moment().format('YYYY-MM-DD')}T00:00:00.000Z`,
+          $lt: `${moment().format('YYYY-MM-DD')}T23:59:59.999Z`,
+        },
+      },
+      { $inc: { click: 1 } },
+      { upsert: true },
+    );
   }
 }
