a1a95442eb1db5507d9852eeeab9a70f5848b109
yeongbin-gong <youngbin816@gmail.com>
2022-06-30 15:25:08 +0900

    queue delete

--- src/postback/postback.module.ts
+++ src/postback/postback.module.ts
@@ -78,9 +78,9 @@ import { PostbackService } from './postback.service';
       { name: Event.name, schema: EventSchema },
       { name: ImpressionCode.name, schema: ImpressionCodeSchema },
     ]),
-    BullModule.registerQueue({
-      name: 'postback',
-    }),
+    // BullModule.registerQueue({
+    //   name: 'postback',
+    // }),
   ],
   controllers: [PostbackController],
   providers: [PostbackService, PostbackConsumer],
