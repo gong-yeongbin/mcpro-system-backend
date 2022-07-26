77acb05c88978b14ea536c2fc74743ef9eb2b50e
yeongbin-gong <youngbin816@gmail.com>
2022-07-26 17:10:39 +0900

    advertiser columne refector

--- src/entities/Advertiser.ts
+++ src/entities/Advertiser.ts
@@ -1,32 +1,20 @@
-import { Column, Entity, Index, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 import { Advertising } from '@entities/Entity';
 
-@Index('IDX_e72a5d292995a75f0f251b7a7d', ['name'], { unique: true })
 @Entity('advertiser', { schema: 'mcpro' })
 export default class Advertiser {
   @PrimaryGeneratedColumn({ type: 'bigint', name: 'idx' })
-  public idx: number;
+  idx: number;
 
-  @Column('varchar', {
-    name: 'name',
-    nullable: true,
-    unique: true,
-    length: 255,
-  })
-  public name: string | null;
+  @Column({ type: 'varchar', name: 'name', nullable: false, unique: true })
+  name: string;
 
-  @Column('datetime', {
-    name: 'created_at',
-    default: () => "'CURRENT_TIMESTAMP(6)'",
-  })
-  public createdAt: Date;
+  @CreateDateColumn({ type: 'timestamp', name: 'createdAt', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
 
-  @Column('datetime', {
-    name: 'updated_at',
-    default: () => "'CURRENT_TIMESTAMP(6)'",
-  })
-  public updatedAt: Date;
+  @UpdateDateColumn({ type: 'timestamp', name: 'updatedAt', default: () => 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
 
   @OneToMany(() => Advertising, (advertising) => advertising.advertiser)
-  public advertising: Advertising[];
+  advertising: Advertising[];
 }
