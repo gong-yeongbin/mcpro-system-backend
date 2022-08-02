49008b5ef026654a06d215f05fbfa7af70e67a7a
yeongbin-gong <youngbin816@gmail.com>
2022-08-02 13:24:09 +0900

    impressionCode

--- src/middleware/impressionCode-cache.middleware.ts
+++ src/middleware/impressionCode-cache.middleware.ts
@@ -5,10 +5,17 @@ import { Redis } from 'ioredis';
 import { v4 } from 'uuid';
 import { Queue } from 'bull';
 import { InjectQueue } from '@nestjs/bull';
+import { ImpressionCode, ImpressionCodeDocument } from 'src/schema/impressionCode';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class ImpressionCodeCacheMiddleware implements NestMiddleware {
-  constructor(private readonly redisService: RedisService, @InjectQueue('impressionCode') private readonly impressionCodeQueue: Queue) {}
+  constructor(
+    private readonly redisService: RedisService,
+    @InjectQueue('impressionCode') private readonly impressionCodeQueue: Queue,
+    @InjectModel(ImpressionCode.name) private readonly impressionCodeModel: Model<ImpressionCodeDocument>,
+  ) {}
 
   async use(request: any, response: any, next: NextFunction): Promise<void> {
     const token: string = request.query.token;
@@ -16,28 +23,30 @@ export class ImpressionCodeCacheMiddleware implements NestMiddleware {
     const sub_id: string = request.query.sub_id;
 
     const redis: Redis = this.redisService.getClient();
+    const isImpressionCodeValidation: string = await redis.get(`${token}:${pub_id}:${sub_id}`);
 
-    let viewCode: string = await redis.hget('view_code', `${token}/${pub_id}/${sub_id}`);
-    if (!viewCode) {
-      viewCode = v4().replace(/-/g, '');
-      await redis.hset('view_code', `${token}/${pub_id}/${sub_id}`, viewCode);
-    }
+    if (!isImpressionCodeValidation) {
+      let viewCode: string = await redis.hget('view_code', `${token}/${pub_id}/${sub_id}`);
+      if (!viewCode) {
+        viewCode = v4().replace(/-/g, '');
+        await redis.hset('view_code', `${token}/${pub_id}/${sub_id}`, viewCode);
+      }
 
-    const impressionCode: string = await redis.get(`${token}:${pub_id}:${sub_id}`);
-
-    if (!impressionCode) {
-      await redis.set(`${token}:${pub_id}:${sub_id}`, viewCode);
-      await redis.expire(`${token}:${pub_id}:${sub_id}`, 60 * 60 * 12);
+      const impressionCodeInstance: ImpressionCode = await this.impressionCodeModel.findOne({ token: token, pub_id: pub_id, sub_id: sub_id }).exec();
+      const impressionCode: string = impressionCodeInstance ? impressionCodeInstance.impressionCode : viewCode;
 
       await this.impressionCodeQueue.add(
         {
           token: token,
           pub_id: pub_id,
           sub_id: sub_id,
-          impressionCode: viewCode,
+          impressionCode: impressionCode,
         },
         { removeOnComplete: true, removeOnFail: true, attempts: 2 },
       );
+
+      await redis.set(`${token}:${pub_id}:${sub_id}`, impressionCode);
+      await redis.expire(`${token}:${pub_id}:${sub_id}`, 60 * 60 * 12);
     }
 
     next();
